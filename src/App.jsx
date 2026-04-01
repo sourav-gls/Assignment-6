226f190dcf427ca83d87fc00a870ec017d080442
sourav-gls <souravghosh.gls@gmail.com>
2026-04-01 13:19:36 +0600

    load data on ui from JSON data

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,19 @@ import Banner from './components/banner/banner'
 import Count from './components/countSection/count'
 import Footer from './components/footer/footer'
 import GetStarted from './components/getStarted/getStarted'
+import Models from './components/models/models'
 import Navbar from './components/navbar'
 import Pricing from './components/pricing/pricing'
+
+
+
+const getModels = async() =>{
+  const res = await fetch("/models.json")
+  return res.json();
+}
+
+const modelPromise = getModels();
+
 
 function App() {
   
@@ -15,6 +26,7 @@ function App() {
     <Navbar></Navbar>
     <Banner></Banner>
     <Count></Count>
+    <Models modelPromise={modelPromise} ></Models>
     <GetStarted></GetStarted>
     <Pricing></Pricing>
     <Footer></Footer>
